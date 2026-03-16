ff973f109e7501d432a8fd1f200b5c0e0b3e3a7c
Kevin Shi <shiliuyu@bu.edu>
2026-03-16 14:06:51 -0400

    v3 Student 1 Part

--- project_2_chatbot/basic_chatbot/src/solution/v3.rs
+++ project_2_chatbot/basic_chatbot/src/solution/v3.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, hash::Hash};
+
 use kalosm::language::*;
 
 #[allow(dead_code)]
@@ -8,13 +10,21 @@ pub struct ChatbotV3 {
     // together!
     // Need to store one chat session per user.
     // Think of some kind of data structure that can help you with this.
+    model: Llama,
+    usernames: HashMap<String, Chat<Llama>>
+
 }
 
 impl ChatbotV3 {
     #[allow(dead_code)]
     pub fn new(model: Llama) -> ChatbotV3 {
+        let chat_session = model
+            .chat()
+            .with_system_prompt("The assistant will act like a pirate");
         return ChatbotV3 {
             // Make sure you initialize your struct members here
+            model,
+            usernames: HashMap::new()
         };
     }
 
@@ -24,7 +34,22 @@ impl ChatbotV3 {
         // Notice, you are given both the `message` and also the `username`.
         // Use this information to select the correct chat session for that user and keep it
         // separated from the sessions of other users.
-        return String::from("Hello, I am not a bot (yet)!");
+        if !self.usernames.contains_key(&username) {
+            let chat_session = self
+                .model
+                .chat()
+                .with_system_prompt("The assistant will act like a pirate");
+
+            self.usernames.insert(username.clone(), chat_session);
+        }
+
+        let chat_session = self.usernames.get_mut(&username).unwrap();
+
+        let asynchronous_output = chat_session.add_message(message);
+        let output = asynchronous_output.await;
+        let output = output.unwrap();
+
+        return output;
     }
 
     #[allow(dead_code)]
