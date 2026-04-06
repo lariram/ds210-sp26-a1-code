8dcf8363de1c61b3e18a211b58f772a144340b67
Kevin Shi <shiliuyu@bu.edu>
2026-04-06 17:17:54 -0400

    make sure everything is uploaded

--- project_3_client_server_analytics/client/src/solution.rs
+++ project_3_client_server_analytics/client/src/solution.rs
@@ -9,17 +9,23 @@ pub async fn run_hello(rpc_client: &RPCInterfaceClient) {
 
 pub async fn run_slow_rpc(rpc_client: &RPCInterfaceClient, query: Query) -> Dataset {
     println!("using slow_rpc");
-    
-    // let dataset = rpc_client.slow_rpc().await.unwrap();
-    todo!("Implement this");
+
+    // get the dataset back from the server:
+    let dataset = rpc_client.slow_rpc(Context::current()).await.unwrap();
 
     // What should you do to the dataset?
     // Hint: you have not used `query`, maybe you need to use it somehow?
+    
+    // process the dataset and query using the compute query on dataset function
+    let output_dataset = compute_query_on_dataset(&dataset, &query);
+
+    // returnt the output dataset:
+    return output_dataset
 }
 
 pub async fn run_fast_rpc(rpc_client: &RPCInterfaceClient, query: Query) -> Dataset {
     println!("using fast_rpc");
 
     // You should call fast_rpc here and not slow_rpc.
-    todo!("Implement this");
+    rpc_client.fast_rpc(Context::current(), query).await.unwrap()
 }
