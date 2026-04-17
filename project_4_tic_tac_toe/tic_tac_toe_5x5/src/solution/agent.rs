6c85b0fc09fa7e7f70c2295e96e80b507f9100a0
Kevin Shi <shiliuyu@bu.edu>
2026-04-17 00:06:30 -0400

    Step 1 Calibration done

--- project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
@@ -11,6 +11,62 @@ impl Agent for SolutionAgent {
     // where <score> is your estimate for the score of the game
     // and <x>, <y> are the position of the move your solution will make.
     fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
-        unimplemented!("Not yet implemented")
+        // If you want to make a recursive call to this solution, use
+        // `SolutionAgent::solve(...)`
+        if board.game_over() {
+            let score = board.score();
+            return (score, 0, 0);
+        }
+
+        // Initialize "best" depending on whether we are maximizing or minimizing:
+        let mut best_score = match player {
+            Player::X => i32::MIN, // X wants to maximize, so it should start from minium
+            Player::O => i32::MAX, // O wants to minimize, so it should start from maximum.
+        };
+
+        // generate a vector containing all possible moves:
+        let all_moves = board.moves();
+
+        // initialize the best move as the first move for now:
+        let mut best_move = all_moves[0];
+
+        // use a for loop to iterate every moves:
+        for mv in all_moves {
+            // Clone so we do not destroy the original board state:
+            let mut next_board = board.clone();
+
+            // apply the move with my player:
+            next_board.apply_move(mv, player);
+
+            // Recurse for the opponent. How?
+            let (score, x, y) =
+                SolutionAgent::solve(&mut next_board, player.flip(), _time_limit);
+
+            // update the best moves data:
+            match player {
+
+                // if the player is X:
+                Player::X => {
+
+                    // if score is higher than the old best moves, replace it:
+                    if score > best_score {
+                        best_score = score;
+                        best_move = mv;
+                    }
+                }
+
+                // if the Player is O:
+                Player::O => {
+                    // if score is higher than the old best moves, replace it:
+                    if score < best_score {
+                        best_score = score;
+                        best_move = mv;
+                    }
+                }
+            }
+        }
+
+        let (x, y) = best_move;
+        return (best_score, x, y);
     }
 }
